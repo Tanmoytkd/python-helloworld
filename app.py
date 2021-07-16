794bdecc2a79b7f63cfc8f57061b684097252079
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2021-07-16 19:56:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! This is TKD!!!"
+    return "Hello World! This is TKD!!! I just discovered deployment pull policy"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
